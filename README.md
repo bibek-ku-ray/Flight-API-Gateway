1a65ac77530f5d197b98f70461776822d838d2d7
Bibek Ray <bi.ray1225@gmail.com>
2024-10-16 07:06:10 +0545

    FEAT: Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flight Management System
+
+This **Flight Booking Service** is  module within the **Flight Management System.**
+
+## Modules
+- [Flight Search Service](https://github.com/bibek-ku-ray/Flight-Service)
+- [Flight Booking Service](https://github.com/bibek-ku-ray/Flight-Booking-Service)
+- [Flight API Gateway](https://github.com/bibek-ku-ray/Flight-API-Gateway)
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const apiRouter  = require("./routes")
+const {ServerConfig, Logger} = require("./config")
+
+const app = express();
+console.log("Bibek ray");
+
+app.use(express.json())
+app.use(express.urlencoded({extends:true}))
+
+app.use('/api', apiRouter)
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`server started running on ${ServerConfig.PORT}`);
+});
