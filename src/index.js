9a5368402cc51c250d1f36df79d742ebef950729
Bibek Ray <bi.ray1225@gmail.com>
2024-10-19 22:40:53 +0545

    feat: Add express-rate-limit middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const apiRouter  = require("./routes")
-const {ServerConfig, Logger} = require("./config")
+const rateLimit = require("express-rate-limit");
+
+const apiRouter = require("./routes");
+const { ServerConfig } = require("./config");
 
 const app = express();
 console.log("Bibek ray");
 
-app.use(express.json())
-app.use(express.urlencoded({extends:true}))
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
+});
 
-app.use('/api', apiRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
+app.use(limiter);
+
+app.use("/api", apiRouter);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`server started running on ${ServerConfig.PORT}`);
