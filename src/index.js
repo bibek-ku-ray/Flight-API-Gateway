acdbd0e6bc68c11e587ad18a6e097ed801252c86
Bibek Ray <bi.ray1225@gmail.com>
2024-10-20 22:11:14 +0545

    feat: Add http-proxy-middleware for flight and booking services

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
+const {createProxyMiddleware} = require ('http-proxy-middleware');
 
 const apiRouter = require("./routes");
 const { ServerConfig } = require("./config");
@@ -9,13 +10,41 @@ console.log("Bibek ray");
 
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000, // 2 minutes
-    limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
+    limit: 10, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
+
+/*
+from here we can access flight service
+so, 
+    http://localhost:3001/flightService/api/v1/info 
+will
+    http://localhost:3000/api/v1/info 
+*/
+app.use(
+    "/flightService",
+    createProxyMiddleware({
+        target: ServerConfig.FLIGHT_SERVICE,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/flightService": "", // Remove '/flightService' from the path
+        },
+    })
+);
+app.use(
+    "/bookingService",
+    createProxyMiddleware({
+        target: ServerConfig.BOOKING_SERVICE,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/bookingService": "", // Remove '/bookingService' from the path
+        },
+    })
+);
 
 app.use("/api", apiRouter);
 
